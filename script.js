0de1d8a56d98b0d4b578c4e17c1b3445786df085
Olgayakubovich81 <148339945+Olgayakubovich81@users.noreply.github.com>
2023-11-08 22:52:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const Num = +prompt("Введите первое число")
+console.log(Num/10)
+
+
+let firstNum = +prompt("Введите первое число")
+let secondNum = +prompt("Введите второе число")
+
+if (firstNum > secondNum) {
+  console.log(secondNum);
+} else {
+  console.log(firstNum);}
+
+
+
+  let num = +prompt("Введите число")
+if (num === 100) {
+  console.log("число равно 100")
+} else if (num < 100) {
+  console.log("число меньше 100")
+} else if (num > 100){
+  console.log("число больше 100")}
+
+
+let userName = prompt("Напишите своё имя.")
+let userAge = +prompt ("Напишите свой возраст." )
+if (userAge > 18) {
+    console.log("Hello " + userName)
+  } else {
+    console.log("Hi " + userName)
+  }
+
+
+
