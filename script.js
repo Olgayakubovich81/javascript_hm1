cf3847da1f40546b4f0e6d31a41385a50cc85d1f
Olgayakubovich81 <148339945+Olgayakubovich81@users.noreply.github.com>
2023-11-11 15:53:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-const Num = +prompt("Введите первое число")
-console.log(Num/10)
+// // Напишите цикл for, который выводит консоль каждое второе число от 0 до 10
 
 
-let firstNum = +prompt("Введите первое число")
-let secondNum = +prompt("Введите второе число")
-
-if (firstNum > secondNum) {
-  console.log(secondNum);
-} else {
-  console.log(firstNum);}
+// // for(let num=0; num<=10; num +=2){
+// //        console.log(num);}
 
 
+// 	//  Напишите цикл for, который выводит в консоль  все числа от 55 до 20
+// 	// for (let num = 55; num >=20; num --)
+// 	// 	{console.log(num);}
 
-  let num = +prompt("Введите число")
-if (num === 100) {
-  console.log("число равно 100")
-} else if (num < 100) {
-  console.log("число меньше 100")
-} else if (num > 100){
-  console.log("число больше 100")}
+	
+// 	//  Дан массив numbers. Вывести в консоль все числа из массива
 
-
-let userName = prompt("Напишите своё имя.")
-let userAge = +prompt ("Напишите свой возраст." )
-if (userAge > 18) {
-    console.log("Hello " + userName)
-  } else {
-    console.log("Hi " + userName)
-  }
+		const numbers = [3, 5, 11, 2, 8, 1, 6];
+         for(let i=0; i<numbers.length; i++)   {
+              console.log(numbers[i]);}
 
 
+	// 	// Сформировать новый массив numbers_squared и передать в него все элементы из массива numbers, возведенные в квадрат
+	
+ let numbers_squared = [];
+
+for(let i=0; i<numbers.length; i++){
+    numbers_squared.push(numbers[i]**2);
+}
+console.log(numbers_squared);
+
+
+// // // // Создать переменную last_elem и передать в нее последний элемент из сформированного массива numbers_squared (обратиться к элементу массива по индексу)
+
+let last_elem
+last_elem= numbers_squared.pop ( )
+console.log (last_elem)
+
+
+
+// Дан объект user. Используя данные из объекта, сформировать строку в формате: ‘User’s name is <FIRST_NAME> <LAST_NAME>. He is <AGE> years old’
+
+// const user = {
+// first_name: 'Ivan', 
+// last_name: 'Ivanov', 
+// age: 20, 
+// salary: 500
+// }
+
+//  console.log (`User’s name is ${user.first_name} ${user.last_name}. He is ${user.age} years old.`)
 
